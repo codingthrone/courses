a795730228cbf5d24bdc18ab7c31741cbf705f0c
rbrtbrnschn <robert.bornschein@mailbox.tu-dresden.de>
2021-02-16 18:49:27 +0100

    structure overhaul, build script

---
+++ web-deploy-201/scripts/build.js
@@ -0,0 +1,12 @@
+const asciidoctor = require('asciidoctor')()
+const fs = require("fs");
+const { argv } = require('process');
+const content = fs.readFileSync("./src/README.adoc","ascii");
+const html = asciidoctor.convert(content) // (2)
+
+try {
+  fs.writeFileSync("./build/index.html", html);
+  console.log("Written html to file: index.html\nExiting...")
+} catch(err) {
+  throw new Error("Writing html to file failed.\n Exiting...");
+}

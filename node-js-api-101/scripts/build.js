051245b8396ed4d7859fa284a87441d789f10f9d
rbrtbrnschn <robert.bornschein@mailbox.tu-dresden.de>
2021-03-16 12:57:55 +0100

    almost done

--- node-js-api-101/scripts/build.js
+++ node-js-api-101/scripts/build.js
@@ -1,10 +1,9 @@
 const asciidoctor = require('asciidoctor')()
 const fs = require("fs");
 const content = fs.readFileSync("./src/README.adoc","ascii");
-const html = asciidoctor.convert(content) // (2)
-
+const body = asciidoctor.convert(content) // (2)
 try {
-  fs.writeFileSync("./build/index.html", html);
+  fs.writeFileSync("./build/index.html", body);
   console.log("Written html to file: index.html\nExiting...")
 } catch(err) {
   throw new Error("Writing html to file failed.\n Exiting...");

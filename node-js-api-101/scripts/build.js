56414b1b758eda9e72d7f503812107832b7ce348
rbrtbrnschn <robert.bornschein@mailbox.tu-dresden.de>
2021-03-16 13:01:38 +0100

    added container

--- node-js-api-101/scripts/build.js
+++ node-js-api-101/scripts/build.js
@@ -2,6 +2,7 @@ const asciidoctor = require('asciidoctor')()
 const fs = require("fs");
 const content = fs.readFileSync("./src/README.adoc","ascii");
 const body = asciidoctor.convert(content) // (2)
+const html = "<div style='width: 70vw;position: absolute;left:15vw;'>"+body+"</div>";
 try {
   fs.writeFileSync("./build/index.html", body);
   console.log("Written html to file: index.html\nExiting...")

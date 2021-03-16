e1ea51f2565d7e3db89197c62b087742236b3ab2
rbrtbrnschn <robert.bornschein@mailbox.tu-dresden.de>
2021-03-16 13:03:32 +0100

    added husky

--- node-js-api-101/scripts/build.js
+++ node-js-api-101/scripts/build.js
@@ -4,7 +4,7 @@ const content = fs.readFileSync("./src/README.adoc","ascii");
 const body = asciidoctor.convert(content) // (2)
 const html = "<div style='width: 70vw;position: absolute;left:15vw;'>"+body+"</div>";
 try {
-  fs.writeFileSync("./build/index.html", body);
+  fs.writeFileSync("./build/index.html", html);
   console.log("Written html to file: index.html\nExiting...")
 } catch(err) {
   throw new Error("Writing html to file failed.\n Exiting...");
